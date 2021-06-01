8dffc0f535096887253b05f4acdca50ad0654d52
prathambatra123 <66113773+prathambatra123@users.noreply.github.com>
2021-06-01 21:32:51 +0800

    Update vars.tf

--- module/s3_static_website/vars.tf
+++ module/s3_static_website/vars.tf
@@ -35,3 +35,22 @@ variable "ACCOUNT_NUMBER" {
   type    = string
   description = "Account no"
   }
+
+variable "s3_origin" {
+  type    = string
+  description = "S3 origin id"
+  default = "myS3Origin"
+}
+
+
+variable "role_name" {
+  type    = string
+  description = "Iam role name"
+  default = "iam_for_lambda"
+}
+
+variable "lambda_basic_policy_name" {
+  type    = string
+  description = "Policy name for lambda"
+  default = "lambda_basic_policy"
+}
