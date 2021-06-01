c763067371f31d486b909ba1094e8abd2a92c21e
prathambatra123 <66113773+prathambatra123@users.noreply.github.com>
2021-06-01 21:30:55 +0800

    Update lambda.tf

--- module/s3_static_website/lambda.tf
+++ module/s3_static_website/lambda.tf
@@ -1,12 +1,12 @@
 data "archive_file" "zipit" {
   type        = "zip"
   source_file = "module/s3_static_website/cf.py"
-  output_path = "poc_cf_lambda.zip"
+  output_path = "${var.lambda_name}.zip"
 }
 
 resource "aws_lambda_function" "poc_cf_lambda" {
   filename      = data.archive_file.zipit.output_path
-  function_name = "poc_cf_lambda"
+  function_name = var.lambda_name
   role          = aws_iam_role.iam_for_lambda.arn
   handler = "cf.lambda_handler"
   runtime = "python3.8"
